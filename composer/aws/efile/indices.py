00570697a60ea3784685dcea4b897df99bbc275b
David Bruce Borenstein <david@appliednonprofitresearch.com>
2019-08-11 19:21:08 -0400

    Modified S3 handling and index download concurrency. All 89 tests pass.

--- composer/aws/efile/indices.py
+++ composer/aws/efile/indices.py
@@ -25,32 +25,38 @@ class EfileIndices(Iterable):
         bucket: Bucket = EfileBucket()
         return cls(bucket)
 
-    def _get_for_year(self, year: int) -> Iterator[FilingMetadata]:
+    def _get_for_year(self, year: int) -> List[Dict]:
         object_key: str = _json_index_key(year)
         try:
             raw: str = self.bucket.get_obj_body(object_key)
         except FileNotFoundError:
-            return
+            return []
         as_json: Dict = json.loads(raw)
 
         # The IRS currently includes a single key in its indices. Blow up if that changes.
         assert len(as_json) == 1
         filing_list_key: str = "Filings%i" % year
         filing_list: List = as_json[filing_list_key]
-        for filing_spec in filing_list:
-            yield FilingMetadata.from_json(filing_spec)
+        return filing_list
 
     def __iter__(self) -> Iterator[FilingMetadata]:
         years: Iterator = range(EARLIEST_YEAR, datetime.now().year + 1)
+        #years: Iterator = range(EARLIEST_YEAR, EARLIEST_YEAR + 1)
 
         # The IRS currently provides e-files starting with those filed in 2011. Blow up if that changes.
         assert not self.bucket.exists(_json_index_key(EARLIEST_YEAR - 1))
         assert self.bucket.exists(_json_index_key(EARLIEST_YEAR))
 
         with ThreadPoolExecutor() as executor:
-            futures: Deque[Future] = deque()
+            future_to_year: Dict[Future, int] = {}
+            results: Deque[List[Dict]] = deque()
             for year in years:
                 future: Future = executor.submit(self._get_for_year, year)
-                futures.append(future)
-            for completed_future in as_completed(futures):  # type: Future
-                yield from completed_future.result()
+                future_to_year[future] = year
+            for completed_future in as_completed(future_to_year):  # type: Future
+                logging.info("Finished downloading index for %i" % future_to_year[completed_future])
+                results.append(completed_future.result())
+
+            for result in results:
+                for filing_spec in result:
+                    yield FilingMetadata.from_json(filing_spec)
--- composer/aws/s3.py
+++ composer/aws/s3.py
@@ -1,97 +1,45 @@
 import logging
 import os
-from abc import ABC, abstractmethod
 
 import boto3
 from typing import *
-from datetime import datetime
-
+from multiprocessing import cpu_count
 from mock import MagicMock
 
 from composer.aws.handshake import Handshake
-import botocore.exceptions
+from botocore.errorfactory import ClientError
+from botocore.client import Config
 
 logging.getLogger("botocore.vendored.requests.packages.urllib3").setLevel(logging.WARNING)
 
-def is_notfound(e: botocore.exceptions.ClientError):
-    return e.response['Error']['Code'] in ("404", "NoSuchKey")
-
-class S3Resource:
-    def __init__(self, id: str, secret: str):
-        self.session = boto3.session.Session(
-            aws_access_key_id=id,
-            aws_secret_access_key=secret
-        )
-
-    def s3(self):
-        return self.session.resource("s3")
-
 class Bucket:
-    def __init__(self, resource: S3Resource, name: str):
-        self.s3 = resource.s3()
+    def __init__(self, s3, name: str):
+        self.s3 = s3
         self.name = name
 
-    # Boto3 generates s3.Object on the fly, so can't type go obj
-    # Also can't figure out how to test the not found functionality
-    def _do_with_404(self, key: str, fn: Callable[[Any], Any]) -> Optional[Any]:
-        # noinspection PyUnresolvedReferences
-        try:
-            obj = self.s3.Object(self.name, key)
-            return fn(obj)
-        except botocore.exceptions.ClientError as e:
-            if is_notfound(e):
-                return None
-            else:
-                raise FileNotFoundError("Failed to read key '%s' in bucket '%s'." % (key, self.name)) from e
-
-    def get_size(self, key: str) -> Optional[int]:
-        def fn(obj: Any) -> int: return obj.get()['ContentLength']
-        return self._do_with_404(key, fn)
-
-    def get_last_modified(self, key: str) -> Optional[datetime]:
-        def fn(obj: Any) -> datetime: return obj.last_modified
-        return self._do_with_404(key, fn)
-
-    def get_etag(self, key: str) -> Optional[str]:
-        def fn(obj: Any) -> str:
-            raw = obj.e_tag
-            return raw.replace('"', '')
-        return self._do_with_404(key, fn)
-
     def get_obj_body(self, key: str, encoding: Optional[str]= "utf-8"):
-        def fn(obj):
-            content = obj.get()
-            body = content["Body"]
-            encoded = body.read()
-            if encoding:
-                decoded = encoded.decode(encoding)
-                return decoded
-            return encoded
-
-        return self._do_with_404(key, fn)
-
-    def _filter(self, key: str):
-        bucket = self.s3.Bucket(name=self.name)
-        objs: List = list(bucket.objects.filter(Prefix=key))
-        return objs
+        obj = self.s3.get_object(Bucket=self.name, Key=key)
+        encoded = obj['Body'].read()
+        if encoding:
+            decoded = encoded.decode(encoding)
+            return decoded
+        return encoded
 
     # SO 33842944
     def exists(self, key: str) -> bool:
-        objs: List = self._filter(key)
-        return len(objs) > 0 and objs[0].key == key
-
-    def list_keys(self) -> List[str]:
-        bucket = self.s3.Bucket(name=self.name)
-        objects = bucket.objects
-        all_objects = objects.all()
-        return [resp.key for resp in all_objects]
+        try:
+            self.s3.head_object(Bucket=self.name, Key=key)
+            return True
+        except ClientError:
+            return False
 
 class AuthenticatedBucket(Bucket):
     def __init__(self, handshake: Handshake, bucket_name: str):
         aws_id = handshake.get_aws_key()
         aws_secret = handshake.get_aws_secret()
-        resource = S3Resource(aws_id, aws_secret)
-        super(AuthenticatedBucket, self).__init__(resource, bucket_name)
+        config: Config = Config(max_pool_connections=cpu_count() * 10)
+        client = boto3.client('s3', aws_access_key_id=aws_id, aws_secret_access_key=aws_secret, config=config)
+        super(AuthenticatedBucket, self).__init__(client, bucket_name)
 
 def file_backed_bucket(root_dir: str) -> Bucket:
     """Mock bucket used in tests and fixture creation"""

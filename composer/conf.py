efc8f9bfb80fb938bda0410dbca3777ada6d264f
Kislov Dmitry <kislov@easydan.com>
2019-08-13 14:45:58 +1000

    settings file

---
+++ composer/conf.py
@@ -0,0 +1,21 @@
+# NOTE: Probably appropriate names need taking into account text 
+# what the main package is intended for. 
+
+
+CACHE_SIZE = 4194304
+
+MAX_WORKERS = 4
+
+JSON_FILENAME = "{}.json"
+
+EARLIEST_YEAR = 2011
+
+INDEX_JSON_NAME = "index_{}.json"
+
+FILING_NAME = "Filings{}"
+
+DOWNLOAD_TIMEOUT = 10
+
+UPDATE_TIMEOUT = 10
+
+PUBLIC_XML_NAME = "{}_public.xml"
